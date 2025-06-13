e837f9d453f77ba66ba04e3455ce9b17d0322959
Sukhankar <126391408+Sukhankar@users.noreply.github.com>
2025-06-13 09:11:50 +0000

    optimized the script file and made the ui funky

--- index.js
+++ index.js
@@ -3,32 +3,30 @@ let cat = document.getElementById("cat");
 const start = document.getElementById("start");
 const stop = document.getElementById("stop");
 const audio = document.getElementById("audio");
-const audio = document.getElementById("audio");
+const audio1 = document.getElementById("audio1");
 const cryaudio = document.getElementById("cryaudio");
 
 const preloadedCatImage = new Image();
 preloadedCatImage.src = "./happy-cat.gif";
 
-function colorFind() {
-  const colorCode = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += colorCode[Math.floor(Math.random() * 16)];
-  }
-  return color;
+function getRandomColor() {
+  return `#${Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, "0")}`;
 }
-let intervalId;
+
+let intervalId = null;
+
 start.addEventListener("click", () => {
   discoBall.src = "./glowball.gif";
   cat.src = preloadedCatImage.src;
 
   audio.src = "./song.mp3";
-  audio1.src="./SpotifyMate.com-Blinding Lights-The Weeknd.mp3";
+  audio1.src = "./SpotifyMate.com-Blinding Lights-The Weeknd.mp3";
   document.querySelector("h4").innerText = "press Stop to End";
   console.log("start");
+
   if (!intervalId) {
     intervalId = setInterval(() => {
-      document.body.style.backgroundColor = colorFind();
+      document.body.style.backgroundColor = getRandomColor();
     }, 200);
   }
 });
@@ -40,7 +38,7 @@ stop.addEventListener("click", () => {
   document.body.style.backgroundColor = "#181616";
   discoBall.src = "./offball.png";
   cat.src = "./banana-cat.gif";
-  audio.src = " ";
-  ausio1.src=" ";
+  audio.src = "";
+  audio1.src = "";
   cryaudio.src = "./crysong.mp3";
 });
